40722bed741d103bf60cacb68359de7cd1f5d4e9
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-18 09:54:54 +0300

    add GroupsView endpoint to API for managing user groups

--- server/myserver/api/urls.py
+++ server/myserver/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterUser
+from .views import RegisterUser , GroupsView
 
 
 urlpatterns = [
     path("register/", RegisterUser.as_view(), name="register"),
+    path("groups/", GroupsView.as_view(), name="groups"),
+
 ]
+
